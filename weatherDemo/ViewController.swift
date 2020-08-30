0f811ffba4e4d61d1c81a3b2254693f91c9a9342
Anton Brinda <anton.brinda@ebcont.com>
2020-08-30 13:19:10 +0200

    add base project

---
+++ weatherDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherDemo
+//
+//  Created by dies irae on 30/08/2020.
+//  Copyright © 2020 dies irae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
